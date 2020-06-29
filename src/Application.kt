603ad2e22974db395972cbffb1876502f5c7fd11
Yoav Luft <yoav.luft@prototyp.se>
2020-06-29 08:39:45 +0200

    Move service discovery code to ServicesManager.kt

--- src/Application.kt
+++ src/Application.kt
@@ -5,7 +5,6 @@ import ch.qos.logback.classic.LoggerContext
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.application.log
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DataConversion
 import io.ktor.gson.gson
@@ -17,7 +16,6 @@ import io.ktor.http.cio.websocket.timeout
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
-import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.websocket.webSocket
 import org.slf4j.LoggerFactory
@@ -25,10 +23,6 @@ import java.net.InetAddress
 import java.net.NetworkInterface
 import java.text.DateFormat
 import java.time.Duration
-import javax.jmdns.JmDNS
-import javax.jmdns.ServiceEvent
-import javax.jmdns.ServiceInfo
-import javax.jmdns.ServiceListener
 
 inline class ServiceName(val name: String)
 
@@ -41,28 +35,6 @@ fun getLocalAddresses(): Iterable<InetAddress> =
         .map { it.address }
 
 data class StatusResponse(val status: String)
-
-data class BitsService(val address: String, val port: Int, val interfaces: List<Pair<String, String>>) {
-    companion object {
-        fun from(serviceInfo: ServiceInfo): BitsService =
-            BitsService(serviceInfo.hostAddresses.first(), serviceInfo.port, parseInterfaces(serviceInfo.textBytes))
-
-        private fun parseInterfaces(textBytes: ByteArray?): List<Pair<String, String>> =
-            textBytes?.let { parseBonjourTxtRecord(it) }.orEmpty()
-
-        private fun parseBonjourTxtRecord(bytes: ByteArray): List<Pair<String, String>> =
-            bytes.toList().breakWith {
-                val length = it.first().toUShort().toInt()
-                Pair(it.drop(1).take(length), it.drop(length + 1))
-            }.map {
-                it.toByteArray().toString(Charsets.UTF_8)
-            }.map {
-                it.split("=")
-            }.map {
-                Pair(it.first(), it.drop(1).joinToString(""))
-            }
-    }
-}
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
@@ -87,10 +59,13 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 
-    val mDNSInstance: JmDNS = getLocalAddresses().first().let { JmDNS.create(it) }
-    val services: MutableMap<ServiceName, BitsService> = mutableMapOf()
-
-    defaultDiscoveryServices().forEach { registerServiceListeners(mDNSInstance, it, services) }
+    val servicesManager =
+        ServicesManager(
+            getLocalAddresses().first(),
+            defaultDiscoveryServices().map { ServiceName(it) },
+            defaultDiscoveryServices().associate {
+                ServiceName(it) to AdvertiseServerData(defaultOscPort(), mapOf("server" to ""))
+            })
 
     routing {
         get("/") {
@@ -102,13 +77,11 @@ fun Application.module(testing: Boolean = false) {
         }
 
         get("/discover") {
-            call.respond(services)
+            call.respond(servicesManager.services)
         }
 
-        post("/discover") {
-            val service = call.request.queryParameters["service"]
-            registerServiceListeners(mDNSInstance, service, services)
-            call.respond(StatusResponse("ok"))
+        webSocket("/connect/{service}") {
+
         }
 
         webSocket("/bitsws/echo") {
@@ -123,35 +96,9 @@ fun Application.module(testing: Boolean = false) {
     }
 }
 
-private fun Application.registerServiceListeners(
-    mDNSInstance: JmDNS,
-    service: String?,
-    services: MutableMap<ServiceName, BitsService>
-) {
-    log.debug("Listen to new service $service")
-    mDNSInstance.addServiceListener(service, object : ServiceListener {
-        override fun serviceResolved(event: ServiceEvent?) {
-            log.info("Service $service resolved on ${mDNSInstance.name}: ${event}")
-            event?.run { services.put(ServiceName(event.name), BitsService.from(event.info)) }
-        }
-
-        override fun serviceRemoved(event: ServiceEvent?) {
-            log.info("Service $service removed on ${mDNSInstance.name}: ${event}")
-            event?.run { services.remove(ServiceName(event.name)) }
-        }
-
-        override fun serviceAdded(event: ServiceEvent?) {
-            log.info("Service $service added on ${mDNSInstance.name}: ${event}")
-            event?.run { services.putIfAbsent(ServiceName(event.name), BitsService.from(event.info)) }
-        }
-    })
-}
-
 private fun Application.defaultDiscoveryServices() =
     environment.config.property("ktor.application.defaultDiscoveryServices").getList()
 
-fun <T> List<T>.breakWith(selector: (List<T>) -> Pair<List<T>, List<T>>): List<List<T>> =
-    if (this.isNotEmpty()) {
-        val (part, rest) = selector(this)
-        listOf(part) + rest.breakWith(selector)
-    } else listOf()
+private fun Application.defaultOscPort(): Int =
+    environment.config.property("ktor.application.defaultOscPort").getString().toInt()
+
---
+++ src/ServicesManager.kt
@@ -0,0 +1,132 @@
+package se.kth.somabits
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.LoggerContext
+import org.slf4j.LoggerFactory
+import java.net.InetAddress
+import javax.jmdns.*
+import kotlin.system.exitProcess
+
+val OSC_SERVICE_TYPE = "_osc._udp.local."
+
+
+val SERVER_SUB_TYPE = "_server"
+
+val log = LoggerFactory.getLogger(ServicesManager::class.qualifiedName)!!
+
+data class AdvertiseServerData(
+    val port: Int,
+    val properties: Map<String, String> = emptyMap(),
+    val weight: Int = 0,
+    val priority: Int = 0
+)
+
+class ServicesManager(
+    val address: InetAddress,
+    val servicesToUse: Iterable<ServiceName>,
+    val advertiseServers: Map<ServiceName, AdvertiseServerData>
+) {
+    val mDNSInstance: JmDNS = JmDNS.create(address)
+    val services: MutableMap<ServiceName, BitsService> = mutableMapOf()
+
+    init {
+        servicesToUse.forEach {
+            mDNSInstance.addServiceListener(it.name, object : ServiceListener {
+                override fun serviceResolved(event: ServiceEvent?) {
+                    log.debug("Service $it resolved on ${mDNSInstance.name}: $event")
+                    event?.run { services.put(ServiceName(event.name), BitsService.from(event.info)) }
+                }
+
+                override fun serviceRemoved(event: ServiceEvent?) {
+                    log.debug("Service $it removed on ${mDNSInstance.name}: $event")
+                    event?.run { services.remove(ServiceName(event.name)) }
+                }
+
+                override fun serviceAdded(event: ServiceEvent?) {
+                    log.debug("Service $it added on ${mDNSInstance.name}: $event")
+                    event?.run { services.putIfAbsent(ServiceName(event.name), BitsService.from(event.info)) }
+                }
+            })
+        }
+        advertiseServers.entries.forEach { (service, serverData) ->
+            mDNSInstance.registerService(
+                ServiceInfo.create(
+                    service.name,
+                    address.hostAddress,
+                    SERVER_SUB_TYPE,
+                    serverData.port,
+                    serverData.weight,
+                    serverData.priority,
+                    serverData.properties
+                )
+            )
+        }
+    }
+}
+
+fun main(arg: Array<String>) {
+    val loggerContext = LoggerFactory.getILoggerFactory() as LoggerContext
+    loggerContext.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).level = Level.INFO
+    val jmDnsInstance = JmDNS.create()
+    jmDnsInstance.addServiceListener(OSC_SERVICE_TYPE, object : ServiceListener {
+        override fun serviceResolved(event: ServiceEvent?) {
+//            println("Resolved: $event")
+        }
+
+        override fun serviceRemoved(event: ServiceEvent?) {
+            println("Removed: $event")
+        }
+
+        override fun serviceAdded(event: ServiceEvent?) {
+            println("Added: $event")
+        }
+
+    })
+    while (true) {
+        val line = readLine()?.split(" ")
+        when (line?.first()) {
+            "q", "quit" -> exitProcess(0)
+            "l", "list" -> println(jmDnsInstance.list(OSC_SERVICE_TYPE))
+            "p", "print" -> if (line.size > 1) {
+                val serviceName = line.drop(1).joinToString(" ")
+                println("$serviceName:")
+                val serviceInfo = jmDnsInstance.getServiceInfo(OSC_SERVICE_TYPE, serviceName)
+                println(serviceInfo.hostAddresses.joinToString(", "))
+                println(serviceInfo.port)
+                println(serviceInfo.niceTextString)
+            }
+        }
+    }
+}
+
+data class BitsService(val address: String, val port: Int, val interfaces: List<Pair<String, String>>) {
+    companion object {
+        fun from(serviceInfo: ServiceInfo): BitsService =
+            BitsService(
+                serviceInfo.hostAddresses.first(),
+                serviceInfo.port,
+                parseInterfaces(serviceInfo.textBytes)
+            )
+
+        private fun parseInterfaces(textBytes: ByteArray?): List<Pair<String, String>> =
+            textBytes?.let { parseBonjourTxtRecord(it) }.orEmpty()
+
+        private fun parseBonjourTxtRecord(bytes: ByteArray): List<Pair<String, String>> =
+            bytes.toList().breakWith {
+                val length = it.first().toUShort().toInt()
+                Pair(it.drop(1).take(length), it.drop(length + 1))
+            }.map {
+                it.toByteArray().toString(Charsets.UTF_8)
+            }.map {
+                it.split("=")
+            }.map {
+                Pair(it.first(), it.drop(1).joinToString(""))
+            }
+    }
+}
+
+fun <T> List<T>.breakWith(selector: (List<T>) -> Pair<List<T>, List<T>>): List<List<T>> =
+    if (this.isNotEmpty()) {
+        val (part, rest) = selector(this)
+        listOf(part) + rest.breakWith(selector)
+    } else listOf()
