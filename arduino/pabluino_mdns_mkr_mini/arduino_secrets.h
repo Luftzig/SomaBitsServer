2e4217ca1be905238d3e05ac1f71d5a3d190be42
Yoav Luft <yoav.luft@prototyp.se>
2020-07-31 13:03:46 +0200

    Rename to match Pavel's naming

--- arduino/pabluino_mdns_mkr_mini/arduino_secrets.h
+++ arduino/pabluino_mdns_mkr_mini/arduino_secrets.h
@@ -3,12 +3,13 @@
 
 // #define SECRET_SSID "serv2"
 // #define SECRET_PASS "somaserv"
-// #define SECRET_SSID "770 "
-// #define SECRET_PASS "19481948"
-#define SECRET_SSID "Middla2_4Ghz"
-#define SECRET_PASS "middla1312"
+#define SECRET_SSID "770 "
+#define SECRET_PASS "19481948"
+// #define SECRET_SSID "Middla2_4Ghz"
+// #define SECRET_PASS "middla1312"
 #define TCP_LISTENING_PORT 12000
 #define DEVICE_NAME "Pabluino-1-motor"
-#define SERVER_IP "192.168.0.186"
+// #define SERVER_IP "192.168.0.186"
+#define SERVER_IP "10.0.1.5"
 
 #endif // ARDUINO_SECRETS_H
